700c08c6a4b3bde187728ae966b2ad5458980ee9
Shreyr69 <shreyrai69@gmail.com>
2026-02-11 11:42:23 +0530

    added file upload feature

--- routes/artifacts.route.js
+++ routes/artifacts.route.js
@@ -1,13 +1,18 @@
 import express from "express";
-import { createArtifact ,getArtifacts} from "../controllers/artifact.controller.js";
-import { authMiddleware} from "../middlewares/auth.middleware.js";
+import {
+    createArtifact,
+    getArtifacts
+} from "../controllers/artifact.controller.js"
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { authorizeRoles } from "../middlewares/role.middlware.js";
+import { upload } from "../middlewares/upload.middleware.js";
+
 
 const router = express.Router();
 
-/**
- * Protected Artifact APIs
- */
-router.post("/", authMiddleware, createArtifact);
-router.get("/", authMiddleware,authorizeRoles("ADMIN"), getArtifacts);
+router.post("/create",authMiddleware,createArtifact);
+router.post("/createWithFile",authMiddleware,upload.single("file"), createArtifact);
+router.get("/", authMiddleware, getArtifacts);
+
+
 export default router;
--- services/artifact.service.js
+++ services/artifact.service.js
@@ -1,40 +1,31 @@
+
 import Artifact from "../models/artifact.js";
 
-/**
- * Create a new artifact
- */
+// Create a new artifact
 export const createArtifactService = async ({
-  title,
-  content,
-  userId
-}) => {
-  if (!title || !content) {
-    throw new Error("Title and content are required");
-  }
-
-  const artifact = await Artifact.create({
     title,
     content,
-    author: userId
-  });
+    userId,
+    filePath
+}) => {
+    if(!title || !content) {
+        throw new Error('Title and Content are required');
+    }
 
-  return artifact;
+    const artifact = await Artifact.create({
+        title,
+        content,
+        author: userId,
+        media: filePath || null
+    });
+    console.log("Artifact created:", artifact);
+    return artifact;    
 };
 
-
-
-
-
-
-
-
-
 export const getArtifactsService = async ({ userId, role }) => {
-  if (role === "ADMIN") {
-    // Admin sees everything
+  if (role === "ADMIN") { 
     return await Artifact.find().populate("author", "name email role");
   }
 
-  // Non-admin sees only their own artifacts
   return await Artifact.find({ author: userId });
 };

eb2b1e0a486b4d77d8596a310f585010cb53bb56
Shreyr69 <shreyrai69@gmail.com>
2026-02-11 11:51:07 +0530

    fixed dev dependencies

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
 dotenv.config();
 
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
+
+const uploadsDir = path.join(process.cwd(), "uploads");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
 
 connectDB();
 

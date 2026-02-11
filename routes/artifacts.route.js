0aea25d1598773a7b2c4b4ae4ae7dfebcaabb60d
Shreyr69 <shreyrai69@gmail.com>
2026-02-11 15:28:48 +0530

    added rate limitter

--- routes/artifacts.route.js
+++ routes/artifacts.route.js
@@ -4,15 +4,15 @@ import {
     getArtifacts
 } from "../controllers/artifact.controller.js"
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { authorizeRoles } from "../middlewares/role.middlware.js";
 import { upload } from "../middlewares/upload.middleware.js";
+import { apiLimiter } from "../middlewares/rateLimiter.middleware.js";
 
 
 const router = express.Router();
 
 router.post("/create",authMiddleware,createArtifact);
-router.post("/createWithFile",authMiddleware,upload.single("file"), createArtifact);
-router.get("/", authMiddleware, getArtifacts);
+router.post("/createWithFile", authMiddleware, apiLimiter, upload.single("file"), createArtifact);
+router.get("/", authMiddleware, apiLimiter, getArtifacts);
 
 
 export default router;
